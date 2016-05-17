05e42fa830113ea72126dab3345cbde1451a14e6
kaiqianz <kaiqianz@yahoo.com>
2016-05-16 17:01:12 -0700

    [ADD] migrations

---
+++ migrations/2016_05_16_220010_create_friend_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friend_requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('requested_user_id')->unsigned();
+            $table->foreign('requested_user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Scheme::drop('friend_requests');
+    }
+}
