a6c6bba69e32c517c2d2dba05d8b9099b764e365
root <root@letsfae.com>
2017-09-25 09:40:51 +0000

    [ADD]Places model function find

--- Laravel/app/Places.php
+++ Laravel/app/Places.php
@@ -5,16 +5,52 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Phaza\LaravelPostgis\Eloquent\PostgisTrait;
 use Phaza\LaravelPostgis\Geometries\Point;
+use Elasticsearch;
 
 class Places extends Model
 {
-	use PostgisTrait;
+	// use PostgisTrait;
 
-    protected $connection = 'foursquare';
-    protected $table = 'places';
-    public $timestamps = false;
-    protected $postgisFields = [
-        'geolocation' => Point::class,
-    ];
+ //    protected $connection = 'foursquare';
+ //    protected $table = 'places';
+ //    public $timestamps = false;
+ //    protected $postgisFields = [
+ //        'geolocation' => Point::class,
+ //    ];
     
+    public function find($place_id){
+        $data = [
+            "body" => [
+                "query"=> [
+                	"bool"=>[
+                        "should"=>[
+                            "match" => [
+			                  "id" => $place_id
+			                ]
+                        ]
+                    ]
+                ]
+            ],
+            "index" => "foursquare",
+            "type" => "places"
+        ];
+
+
+        $raw_places = Elasticsearch::search($data)['hits']['hits'];
+        $places = array();
+        foreach ($raw_places as $place) {
+            $places[] = $place['_source'];
+        }
+
+        return $places;
+    }
+    public function store($place_id){
+        // Validate the request...
+
+        $flight = new Flight;
+
+        $flight->name = $request->name;
+
+        $flight->save();
+    }
 }
